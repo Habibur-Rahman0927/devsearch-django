f23b8f198b037cb1e0638ddc3ff72699a7bddc46
Habibur-Rahman0927 <habibur.rahaman.0927@gmail.com>
2022-02-25 23:06:29 +0600

    added user app and profile

--- projects/urls.py
+++ projects/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.projects, name='projects'),
-    path('projects/<str:pk>/', views.singleProject, name='single-projects'),
+    path('project/<str:pk>/', views.singleProject, name='single-projects'),
     path('create-project/', views.createProject, name='create-project'),
     path('create-project/<str:pk>/', views.updateProject, name='update-project'),
     path('delete-project/<str:pk>/', views.deleteProject, name='delete-project'),
