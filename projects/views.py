ddb05ad1eec0b8f6c507b2a8efb4418a3db12175
Habibur-Rahman0927 <habibur.rahaman.0927@gmail.com>
2022-02-28 20:32:02 +0600

    project searching updated

--- projects/views.py
+++ projects/views.py
@@ -26,7 +26,7 @@ def createProject(request):
             project = form.save(commit=False)
             project.owner = profile
             project.save()
-            return redirect('projects')
+            return redirect('account')
     context = {'form': form}
     return render(request, 'projects/project_form.html', context)
 
@@ -39,7 +39,7 @@ def updateProject(request, pk):
         form = ProjectForm(request.POST, request.FILES, instance=project)
         if form.is_valid():
             form.save()
-            return redirect('projects')
+            return redirect('account')
     context = {'form': form}
     return render(request, 'projects/project_form.html', context)
 
@@ -50,7 +50,7 @@ def deleteProject(request, pk):
     if request.method == 'POST':
         project.delete()
         messages.success(request, 'Project is deleted successfully!')
-        return redirect('projects')
+        return redirect('account')
     
     context = {'object': project}
     return render(request, 'delete_template.html', context)
