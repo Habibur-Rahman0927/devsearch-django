d6f650df1c24fe5af9c337e6c4b8b03e0aa865cf
Habibur-Rahman0927 <habibur.rahaman.0927@gmail.com>
2022-02-28 19:50:03 +0600

    skill delete

--- users/views.py
+++ users/views.py
@@ -121,3 +121,15 @@ def updateSkill(request, pk):
             return redirect('account')
     context = {'form': form}
     return render(request, 'users/skill_form.html', context)
+
+
+@login_required(login_url='login')
+def deleteSkill(request, pk):
+    profile = request.user.profile
+    skill = profile.skill_set.get(id=pk)
+    if request.method == 'POST':
+        skill.delete()
+        return redirect('account')
+    
+    context = {'object': skill}
+    return render(request, 'delete_template.html', context)
