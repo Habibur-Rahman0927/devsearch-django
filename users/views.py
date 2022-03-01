f276bb54049ea91bafe55ee17a4ab46e9d7ad946
Habibur-Rahman0927 <habibur.rahaman.0927@gmail.com>
2022-03-01 23:11:58 +0600

    updated email sending

--- users/views.py
+++ users/views.py
@@ -5,7 +5,7 @@ from django.contrib import messages
 from django.contrib.auth.models import User
 from django.db.models import Q
 from .models import Profile, Skill, Message
-from .forms import CustomUserCreationForm, ProfileForm, skillForm
+from .forms import CustomUserCreationForm, ProfileForm, skillForm, MessageForm
 from .utils import searchProfile, paginationProfiles
 # Create your views here.
 
@@ -157,3 +157,28 @@ def viewMessage(request, pk):
         message.save()
     context = {'message': message}
     return render(request, 'users/message.html', context)
+
+
+def createMessage(request, pk):
+    recipient = Profile.objects.get(id=pk)
+    form = MessageForm()
+    try:
+        sender = request.user.profile
+    except:
+        sender = None
+    
+    if request.method == 'POST':
+        form = MessageForm(request.POST)
+        if form.is_valid():
+            message = form.save(commit=False)
+            message.sender = sender
+            message.recipient = recipient
+
+            if  sender:
+                message.name = sender.name
+                message.email = sender.email
+            message.save()
+            messages.success(request, 'Your messsage was successfully send')
+            return redirect('user-profiles', pk=recipient.id)
+    context = {'recipient': recipient, 'form': form}
+    return render(request, 'users/message_form.html', context)
